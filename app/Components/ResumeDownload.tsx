add8516dd9dd31e0a0f99fb2010968e3cfea213a
akatz113 <drewkatz23@gmail.com>
2024-07-31 22:07:58 -0400

    resume download file part 1

--- app/Components/ResumeDownload.tsx
+++ app/Components/ResumeDownload.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 
+const resumeFile =
+  "Drew Katz/drew-personal-website/public/Resume - Andrew Katz (2024).pdf";
+
 const ResumeDownload = () => {
   return (
     <div>
-      <button className="btn btn-outline btn-secondary">Download Resume</button>
+      <button className="btn btn-outline rounded-xl btn-secondary">
+        <a href={resumeFile} download="myResume">
+          Download Resume
+        </a>
+      </button>
     </div>
   );
 };
